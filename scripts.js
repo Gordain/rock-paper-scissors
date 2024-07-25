8350b657c114f327cd1321e03f97bb4b46f024ef
Jordan Else <jordan.else@hotmail.co.uk>
2024-07-25 18:51:30 +0100

    Create randomInt function and computerChoice function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-console.log("hello World");
+function getRandomNumber(max){
+    return Math.floor(Math.random()*max);   //get a random number between 0 and max
+}
+
+function getComputerChoice(){
+    let hand;
+    let x = getRandomNumber(3);  //assign random number to variable x, in this case max
+    if(x=0){                     //is 3 as we want a choice of three options
+        return hand = "rock";
+    }else if(x=1){
+        return hand = "paper";
+    }else{
+        return hand = "scissors";
+    }                          
+}
