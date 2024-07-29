d8128ba94985ea94eb06c1a034e0950087785216
Jordan Else <jordan.else@hotmail.co.uk>
2024-07-29 20:48:54 +0100

    Move playRound function inside of playGame function

--- scripts.js
+++ scripts.js
@@ -22,7 +22,7 @@ function getHumanChoice(){      //this will prompt the user to type in their cho
     return humanHand;
 }
 
-function playRound(humanChoice, computerChoice){    //this will play a round with the assigned choices
+/*function playRound(humanChoice, computerChoice){    //this will play a round with the assigned choices
     let text = humanChoice.toLowerCase();    //parses human choice to make it case-insensitive
     let output;
     switch(text){   //nest switch statement to evaluate the humanChoice aginst the computer
@@ -60,11 +60,48 @@ function playRound(humanChoice, computerChoice){    //this will play a round wit
             }
             break;
     }
-}
+}*/
 
 function playGame(){
     for(let i = 0; i < 5; i++){
-
+        playRound(humanChoice,computerChoice){
+            let text = humanChoice.toLowerCase();    
+            switch(text){   
+                case "rock":
+                    if(computerChoice == "rock"){
+                        return console.log("Draw!");                
+                    }else if(computerChoice == "paper"){
+                        computerScore = computerScore++;
+                        return console.log("You lose! Paper beats rock");
+                    }else{
+                        humanScore = humanScore++;
+                        return console.log("You win! Rock beats scissors");
+                    }
+                    break;
+                case "paper":
+                    if(computerChoice == "rock"){
+                        humanScore = humanScore++;
+                        return console.log("You win! Paper beats rock");
+                    }else if(computerChoice == "paper"){
+                        return console.log("Draw!");
+                    }else{
+                        computerScore = computerScore++;
+                        return console.log("You lose! Scissors beats paper");
+                    }
+                    break;
+                case "scissors":
+                    if(computerChoice == "rock"){
+                        computerScore = computerScore++;
+                        return console.log("You lose! Rock beats scissors");
+                    }else if(computerChoice == "paper"){
+                        humanScore = humanScore++;
+                        return console.log("You win! Scissors beats paper");
+                    }else{
+                        return console.log("Draw!");
+                    }
+                    break;
+            }
+        }
     }
 }
 
